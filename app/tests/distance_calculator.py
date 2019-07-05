7e34953f226395da4f67b4b3945a1279d8916c1c
Apoorv IC <ic-task@gmail.com>
2019-07-05 09:52:39 +0530

    Implement distance calculator

--- app/tests/distance_calculator.py
+++ app/tests/distance_calculator.py
@@ -9,5 +9,5 @@ class DistanceCalculatorTest(unittest.TestCase):
     def test_arc_length(self):
         point_1 = [52.833502, -8.522366]
         point_2 = [SEARCH_CENTER_LAT, SEARCH_CENTER_LONG]
-
-        self.assertEqual(get_arc_length(point_1, point_2), 161.4)
+        radius = 6371
+        self.assertEqual(round(get_arc_length(radius, point_1, point_2), 1), 161.4)
--- app/utils/distance_calculator.py
+++ app/utils/distance_calculator.py
@@ -1,16 +1,17 @@
+from math import acos, radians, sin, cos
+
 from config import SEARCH_RADIUS
 
 
 def get_central_angle(point_1, point_2):
-    # delta_lat = point_1[0] - point_2[0]
-    delta_long = point_2[1] - point_2[1]
-    # angle = arccos(sin(point_1[0])*sin(point_2[0]) + cos(point_2[1]) * cos(delta_long))
-    return 0
+    radian_point_1 = [radians(deg) for deg in point_1]
+    radian_point_2 = [radians(deg) for deg in point_2]
+    delta_long = radians(point_2[1] - point_1[1])
+    acos_input = sin(radian_point_1[0]) * sin(radian_point_2[0]) + cos(radian_point_1[0]) * cos(radian_point_2[0]) * cos(delta_long)
+    angle = acos(acos_input)
+    return angle
 
 
-
-def get_arc_length(point_1, point_2):
-    return 0
-    # radius = SEARCH_RADIUS
-    # central_angle = get_central_angle(point_1, point_2)
-    # return radius * central_angle
+def get_arc_length(radius, point_1, point_2):
+    central_angle = get_central_angle(point_1, point_2)
+    return radius * central_angle
